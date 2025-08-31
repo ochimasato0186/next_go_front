1f3d8b77c0d35ffee51fa7afb37080b7ef53959a
Muzin0062 <2301040@s.asojuku.ac.jp>
2025-08-31 22:52:58 +0900

    chore: 変更ファイルを全てコミット

---
+++ app/teacher/calender/page.tsx
@@ -0,0 +1,22 @@
+import SmartphoneFrame from "../../../components/SmartphoneFrame";
+import SmartphoneHeader from "../../../components/SmartphoneHeader";
+import StudentFooter from "../../../components/CommonFooter";
+import Calendar from "../../../components/Calendar";
+import StudentBell from "../../../components/StudentBell"
+
+export default function Home() {
+  return (
+    <div className="min-h-screen flex items-center justify-center" style={{ background: '#fff' }}>
+      <SmartphoneFrame>
+            <SmartphoneHeader />
+            <div style={{ position: 'absolute', top: '25mm', right: '3mm', zIndex: 50 }}><StudentBell count={3} /></div>
+        <main className="flex-1 p-4 flex flex-col items-center justify-center">
+          <div style={{ marginTop: "-210px" }}>
+            <Calendar />
+          </div>
+        </main>
+        <StudentFooter />
+      </SmartphoneFrame>
+    </div>
+  );
+}
